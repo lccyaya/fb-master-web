5ae8d6dc8f15353e3bca6fc1a45c65f18104fd2f
hsj <tank2308635@163.com>
2022-12-28 18:27:57 +0800

    修改banner跳转bug

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -90,6 +90,7 @@ const BannerWidget: React.FC<CarouselProps> = (props) => {
             type: 'global/setHomeActiveKey',
             payload: 'app',
           });
+          history.push(`/zh/home`);
           break;
         default:
           break;
